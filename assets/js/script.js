f333922b3332fb8465cb13ad8c3e3c849695499d
Jake Novelli <jakenovelli11@gmail.com>
2021-04-01 17:49:24 -0700

    added comments on what functions I will use

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,43 @@
 // Display colors on each event throughout the column that will change from green to red once the time of day has passed.
 // Create a function that will relate the user's input with the time-block
 // Create a function that will then relate the time block the user entered with the time of day tracker
-// 
 
-var past = $('.past');
-var present = $('.present');
-var future = $('.future');
-var save = $('.saveBtn');
+var saveBtn = $('.saveBtn');
 var dayTime = $('#currentDay');
-var hour = $('.hour');
 var userInput = $('.description');
 
 // Current time track
 var realTime = moment();
 dayTime.text(realTime.format('MMM DD, YYYY [at] hh:mm:ss a'));
 
+// Set time in hours to compare to name's
+var currentTime = realTime.format('H');
+
+
+
+
+// function saveEvent() {
+//     var userEvent = userInput.value;
+//     localStorage.setItem("userEvent", JSON.stringify(userEvent))
+// };
+
+// function renderLastEvent() {
+//     var lastEvent = JSON.parse(localStorage.getItem("userEvent"));
+
+//     if( lastEvent !== null) {
+//         $('.description').innerHTML = lastEvent.userEvent;
+//     } else {
+//         return;
+//     }
+// };
+
+// saveBtn.on('click', function(event) {
+//     event.preventDefault();
+//     saveEvent();
+//     renderLastEvent();
+// });
+
+// function init() {
+//     renderLastEvent();
+// }
+// init();
